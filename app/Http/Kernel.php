7dd7d49dd83e7d76a1447bb7c46976f1b8485e17
Gustavo Silveira <gdsmp@hotmail.com>
2015-10-01 23:00:01 -0300

    primeiro commit

---
+++ app/Http/Kernel.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace GtcCommerce\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    /**
+     * The application's global HTTP middleware stack.
+     *
+     * @var array
+     */
+    protected $middleware = [
+        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        \GtcCommerce\Http\Middleware\EncryptCookies::class,
+        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        \Illuminate\Session\Middleware\StartSession::class,
+        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        \GtcCommerce\Http\Middleware\VerifyCsrfToken::class,
+    ];
+
+    /**
+     * The application's route middleware.
+     *
+     * @var array
+     */
+    protected $routeMiddleware = [
+        'auth' => \GtcCommerce\Http\Middleware\Authenticate::class,
+        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'guest' => \GtcCommerce\Http\Middleware\RedirectIfAuthenticated::class,
+    ];
+}
---
+++ app/Http/Requests/Request.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace GtcCommerce\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class Request extends FormRequest
+{
+    //
+}

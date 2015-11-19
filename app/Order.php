f3895148175307462a7960fa83451260696e039e
Gustavo Silveira <gdsmp@hotmail.com>
2015-11-19 17:11:49 -0200

    Correção Tags

--- app/Order.php
+++ app/Order.php
@@ -6,22 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    protected $fillable = [
-        'user_id',
-        'total',
-        'status',
-    ];
-
-	// relacionamento entre items e order
-    public function items()
-    {
-    	// Com isso consigo acessar todos os items dessa order.
-    	return $this->hasMany('CodeCommerce\OrderItem');
-    }
-
-    // relacionamento entre Order e Usuário
-    public function user()
-    {
-    	return $this->belongsTo('CodeCommerce\User');
-    }
+    //
 }
--- app/OrderItem.php
+++ app/OrderItem.php
@@ -6,18 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderItem extends Model
 {
-    protected $table = 'order_items';
-
-    protected $fillable = [
-    	'product_id',
-    	'price',
-    	'qtd'
-    ];
-
-    //relacionamento entre items com pedido(order)
-    public function order()
-    {
-    	// digo que esse item pertence a um pedido(order)
-    	return $this->belongsTo('CodeCommerce\Order');
-    }
+    //
 }
